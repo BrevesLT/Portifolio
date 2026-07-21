e786886ec009bf5f1fafa15dc1e4bf21b7fc722c
BrevesLT <alyssonbreves5@gmail.com>
2026-07-21 14:29:47 -0300

    slider de projetos

--- script.js
+++ script.js
@@ -6,3 +6,45 @@ button.addEventListener('click', () => {
   button.textContent = document.documentElement.classList.contains('light') ? '🌙' : '☀️'
   
 });
+
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const projetosContainer = document.querySelector('.projetos-container');
+const projetos = document.querySelectorAll('article');
+const larguraProjeto = projetos[0].offsetWidth + 100;
+
+let projeto = 1;
+let intervalo;
+
+function updateProjeto() {
+  if (projeto > projetos.length) {
+    projeto = 1;
+  } else if (projeto < 1) {
+    projeto = projetos.length;
+  }
+
+  projetosContainer.style.transform = `translateX(-${(projeto - 1) * larguraProjeto}px)`;
+
+  intervalo = setTimeout(() => {
+    projeto++;
+    updateProjeto();
+
+  }, 4000);
+}
+
+prevButton.addEventListener('click', () => {
+  projeto--;
+  clearTimeout(intervalo);
+  updateProjeto();
+});
+
+nextButton.addEventListener('click', () => {
+  projeto++;
+  clearTimeout(intervalo);
+  updateProjeto();
+});
+
+updateProjeto();
+
+
+
